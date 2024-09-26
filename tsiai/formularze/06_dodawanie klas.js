6e463bdbec1f2d1fae25e3fc5a8528fa14f34bef
Jolanta Gryko <jgryko@gmail.com>
2024-09-26 13:51:35 +0200

    js

--- tsiai/formularze/06_dodawanie klas.js
+++ tsiai/formularze/06_dodawanie klas.js
@@ -1,2 +1,22 @@
-const kodE=document.getElementById('code')
-const parE=document.querySelector('#right p') 
+const codeE=document.getElementById('code')
+const parE=document.querySelector('.right p') 
+
+codeE.addEventListener('change',(e)=>{
+    if(codeE.checked){
+        parE.classList.add("code-text") 
+    }
+    else{
+        parE.classList.remove("code-text") 
+    }
+
+})
+
+const fontE=document.querySelector('.font')
+const fontSizeE=document.querySelector('#font-size')
+const unitE=document.querySelector('#unit')
+
+fontE.addEventListener('change',(e)=>{
+    parE.style.fontSize=fontSizeE.value+unitE.value
+})
+
+
