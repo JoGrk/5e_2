85aa45b41a58f05315268917853b8d2abb33068e
Jolanta Gryko <jgryko@gmail.com>
2024-11-26 12:07:42 +0100

    26.11

---
+++ exam/php/INF.03-04-24.06-SG galeria/galeria.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galeria</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Zdjęcia</h1>
+    </header>
+    <section id="left">
+        <h2>Tematy zdjęć</h2>
+        <ol>
+            <li>Zwierzęta</li>
+            <li>Krajobrazy</li>
+            <li>Miasta</li>
+            <li>Przyroda</li>
+            <li>Samochody</li>
+        </ol>
+    </section>
+    <section id="mid">
+        <!-- script 1 -->
+    </section>
+    <section id="right">
+        <h2>Najbardziej lubiane</h2>
+        <!-- script 2 -->
+        <strong>Zobacz wszystkie nasze zdjęcia</strong>
+    </section>
+    <footer>
+    <h5>Stronę wykonał: ja</h5>
+    </footer>
+</body>
+</html>
